e53a90ae93ae2ab2220eb36be49c4545adb20b97
Ilya Zhevlakov <ilya8bojik@gmail.com>
2022-11-12 01:18:09 +0400

    Minor changes to koin docs and inject

--- Sellers Application/app/src/main/java/ru/eg/sellersapplication/presentation/ApplicationModule.kt
+++ Sellers Application/app/src/main/java/ru/eg/sellersapplication/presentation/ApplicationModule.kt
@@ -1,10 +1,22 @@
 package ru.eg.sellersapplication.presentation
 
 import android.app.Application
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import ru.eg.sellersapplication.presentation.di.viewModelsModule
 
+/**
+ * Application class. Need to koin.
+ * @param modules koin injection modules
+ */
 class ApplicationModule: Application() {
     override fun onCreate() {
         super.onCreate()
 
+        startKoin {
+            androidLogger(Level.DEBUG)
+            modules(viewModelsModule) //Так объявляются модули, их пишем как vararg.
+        }
     }
 }
--- Sellers Application/app/src/main/java/ru/eg/sellersapplication/presentation/ui/activities/MainActivity.kt
+++ Sellers Application/app/src/main/java/ru/eg/sellersapplication/presentation/ui/activities/MainActivity.kt
@@ -5,6 +5,14 @@ import android.os.Bundle
 import ru.eg.sellersapplication.R
 
 class MainActivity : AppCompatActivity() {
+    /*
+    Иницализация viewmodel такая:
+    private val someViewModel by viewModel<SomeViewModel>()
+    Возможна инциализация любого модуля через конструктор класса:
+    class SomeClass(private val SomeModule): SomeInterface
+     */
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
